6537aca54f05840016481fc5273ac405fa21ae3c
Raikuro <jugragu22@gmail.com>
2017-04-18 01:31:54 +0200

    [FEAT]AngularClass Completed

--- angular/src/app/permission/permission.ts
+++ angular/src/app/permission/permission.ts
@@ -71,15 +71,46 @@ export class Permission {
   }
 
   set hex(hex:string){
-
+    console.log(this._permission);
+    if(hex.length === 4){
+      this._permission.special.hex = hex[0];
+      this._permission.user.hex = hex[1];
+      this._permission.group.hex = hex[2];
+      this._permission.other.hex = hex[3];
+    }
+    else{
+      if(hex.length > 4){
+        this.hex = hex.slice(hex.length-4, hex.length);
+      }
+    }
   }
   
   set binary(binary:string){
-
+    if(binary.length === 16){
+      this._permission.special.binary = binary.slice(0, 4);
+      this._permission.user.binary = binary.slice(4, 8);
+      this._permission.group.binary = binary.slice(8, 12);
+      this._permission.other.binary = binary.slice(12, 16);
+    }
+    else{
+      if(binary.length > 16){
+        this.binary = binary.slice(binary.length-16, binary.length);
+      }
+    }
   }
 
   set verbose(verbose:string){
-    
+    if(verbose.length === 12){
+      this._permission.special.verbose = verbose.slice(0, 3);
+      this._permission.user.verbose = verbose.slice(3, 6);
+      this._permission.group.verbose = verbose.slice(6, 9);
+      this._permission.other.verbose = verbose.slice(9, 12);
+    }
+    else{
+      if(verbose.length > 12){
+        this.verbose = verbose.slice(verbose.length-12, verbose.length);
+      }
+    }
   }
 
 }
@@ -122,25 +153,50 @@ class PermGroup{
 
   get hex(): string{
     let res='';
-    res += (4*(+this._read) +
-            2*(+this._write) +
-            (+this._exec));
+    res += (4*(+this.read) +
+            2*(+this.write) +
+            (+this.exec));
     return res;
   }
 
   get binary(): string{
     let res = '0';
-    res += (+this._read);
-    res += (+this._write);
-    res += (+this._exec);
+    res += (+this.read);
+    res += (+this.write);
+    res += (+this.exec);
     return res;
   }
 
   get verbose(): string{
-      let res = '';
-      res += (this._read) ? 'r' : '-';
-      res += (this._write) ? 'w' : '-';
-      res += (this._exec) ? 'x' : '-';
-      return res;
+    let res = '';
+    res += (this.read) ? 'r' : '-';
+    res += (this.write) ? 'w' : '-';
+    res += (this.exec) ? 'x' : '-';
+    return res;
+  }
+
+  set hex(hex){
+    let aux = parseInt(hex);
+    if(aux > 3){
+      this.read = true;
+      aux-=4;
     }
+    if(aux>1){
+      this.write = true;
+      aux-=2;
+    }
+    this.exec = (aux>0);
+  }
+
+  set binary(binary){
+    this.read = (binary[2]==='1');
+    this.write = (binary[1]==='1');
+    this.exec = (binary[0]==='1');
+  }
+
+  set verbose(verbose){
+    this.read = (verbose[2]==='r');
+    this.write = (verbose[1]==='w');
+    this.exec = (verbose[0]==='x');
+  }
 }
