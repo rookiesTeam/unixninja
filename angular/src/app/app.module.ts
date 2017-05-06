45fb8f7514175729a630cacfe7a03c9a589c2301
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2017-05-06 23:01:34 +0200

    feat(angular): add ClipboardModule into AppModule
    Start implementation of own copy2clipboard component using ClipboardService

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClipboardModule } from 'ng2-clipboard/ng2-clipboard'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
