ca42b83afba1d3c6020b2f0986b9f70891f18486
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2017-05-07 03:38:13 +0200

    feat(angular): add copy to clipboard button at interface inputs and outputs
    Finished implementation of button, which allows user to copy binary, hexadecimal, and other
    permission representation to clipboard. Refactor previous div `Copy` to a button inspired on
    Github's one. More info at #7
    
    7

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ClipboardModule } from 'ng2-clipboard/ng2-clipboard'
+import { ClipboardModule } from 'ng2-clipboard/ng2-clipboard';
+import { CopyToClipboardComponent } from './copy-to-clipboard/copy-to-clipboard.component';
+import { PopoverModule } from 'ng2-pop-over';
+import { PopoverButtonComponent } from './copy-to-clipboard/popover-button/popover-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CopyToClipboardComponent,
+    PopoverButtonComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ClipboardModule
+    ClipboardModule,
+    PopoverModule
   ],
   providers: [],
   bootstrap: [AppComponent]
